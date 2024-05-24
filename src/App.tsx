ceab2fad25bc8827ca1b5e4762d728e72b843c49
Zygimantas Vanagas <zygimantux@gmail.com>
2024-05-24 12:49:19 +0300

    Add tailwind & table for displaying data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
+import { useState } from 'react';
 import './App.css';
+import { GildedRose, Item } from './lib/gilded-rose/gilded-rose';
+import {
+  AGED_BRIE,
+  BACKSTAGE_PASSES,
+  SULFURAS,
+} from './lib/gilded-rose/constants/items';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './components/ui/table';
+import { Button } from './components/ui/button';
 
 function App() {
-  return <></>;
+  const [items, setItems] = useState<Item[]>([
+    new Item(AGED_BRIE, 4, 2),
+    new Item(SULFURAS, 4, 2),
+    new Item(BACKSTAGE_PASSES, 15, 2),
+  ]);
+
+  const gildedRose = new GildedRose(items);
+
+  const renderItems = () => (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Sell In</TableHead>
+          <TableHead>Quality</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {gildedRose.items.map((item, index) => (
+          <TableRow key={index}>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>{item.sellIn}</TableCell>
+            <TableCell>{item.quality}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      {renderItems()}
+      <Button
+        onClick={() => {
+          const updatedItems = gildedRose.updateQuality();
+          setItems([...updatedItems]);
+        }}
+      >
+        Update
+      </Button>
+    </div>
+  );
 }
 
 export default App;
